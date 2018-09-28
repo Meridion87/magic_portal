ff709d74130ac7f9bfde6fe69ab96ac55a3ab837
carlos <carlos.molina.munoz@gmail.com>
2018-09-28 20:40:21 -0300

    después de agregar página de perfil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   # If your model is called User
-  def after_sign_in_path_for(resource)
-    session["user_return_to"] || root_path
+  def after_sign_in_path_for(user)
+    user_path(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+
+
   resources :cards
 
   devise_for :users
 
+  resources :users
 
   root 'cards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
