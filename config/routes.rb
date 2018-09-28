0573cad3eb9036ec52e301f3c3268ee5e8550fef
carlos <carlos.molina.munoz@gmail.com>
2018-09-28 20:28:33 -0300

    antes de crear un usuario con scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :cards
 
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  devise_for :users
 
 
   root 'cards#index'
